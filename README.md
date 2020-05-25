52a10d3346589cf50e4eb1211667b910eae765b5
Stéphanie <33481933+btwSte@users.noreply.github.com>
2020-05-25 19:32:21 -0300

    css mobile add

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rock In Rio 85
+
+Projeto interdisciplinar do 3° semestre do curso de Design Digital
---
+++ js/eventos.js
@@ -0,0 +1,8 @@
+// Abrir e fechar menu 
+$(".btn_menu").click(function() {
+    $(".menu").show();
+});
+
+$(".btn_close").click(function() {
+    $(".menu").hide();
+});
